0c2668ab696d224afb093ae116005733ee9d509d
duiliob02 <duilio.b02@gmail.com>
2024-05-17 17:14:05 +0200

    Inserita Keyboard custom

--- SpectrumStories/ContentView.swift
+++ SpectrumStories/ContentView.swift
@@ -8,16 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nameText : String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Rectangle()
+                .fill(Color.giallio)
+                .ignoresSafeArea()
+            VStack {
+                Text("WHAT'S YOUR NAME?")
+                    .font(.custom("Coiny-Regular", size: 60))
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    TextField("", text: $nameText)
+                        .font(.custom("Coiny-Regular", size: 50))
+                        .frame(width: UIScreen.main.bounds.width / 2, height: 80)
+                        .padding()
+                        .background {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(.white)
+                        }
+                    Button(action: {}, label: {Image(systemName: "circle.fill")})
+                }
+                .padding(.bottom)
+                
+                ForEach(0..<KeyBoard._keyboard.count, id: \.self) { rowIndex in
+                    HStack {
+                        ForEach(0..<KeyBoard._keyboard[rowIndex].count, id: \.self) { columnIndex in
+                            Text(KeyBoard._keyboard[rowIndex][columnIndex])
+                                .padding()
+                                .background(Color.giallio2)
+                                .cornerRadius(5)
+                                .onTapGesture {
+                                    nameText.append(KeyBoard._keyboard[rowIndex][columnIndex])
+                                }
+                        }
+                    }
+                }
+                
+                HStack{
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.2))
+                        .frame(width: UIScreen.main.bounds.width / 3.5, height: 50)
+                        .onTapGesture {
+                            nameText.append(" ")
+                        }
+                    Image(systemName: "delete.left")
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .onTapGesture {
+                            _ = nameText.popLast()
+                        }
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
+
+struct KeyBoard {
+    static let _keyboard = [
+        ["Q", "W", "E","R","T","Y","U","I","O","P"],
+        ["A","S","D","F","G","H","J","K","L"],
+        ["Z","X","C","V","B","N","M"]
+    ]
+}
+
 
 #Preview {
     ContentView()
