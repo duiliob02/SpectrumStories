fc9566fa323758d5274b7975067e6b1d4d852574
duiliob02 <duilio.b02@gmail.com>
2024-05-17 16:14:29 +0200

    Initial Commit

---
+++ SpectrumStories/SpectrumStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectrumStoriesApp.swift
+//  SpectrumStories
+//
+//  Created by Duilio Barbato on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SpectrumStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
